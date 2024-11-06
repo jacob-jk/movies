b6352fd5f00620cffb69ae40384956d174ebe1ed
Kuba Karnath <jacob_jk@wp.pl>
2024-11-06 12:31:47 +0100

    GlobalStyle and theme added to project

---
+++ src/theme.js
@@ -0,0 +1,17 @@
+export const theme = {
+  color: {
+    white: "#FFFFFF",
+    mystic: "#E4E6F0",
+    whisper: "#F5F5FA",
+    pattensBlue: "#D6E4FF",
+    scienceBlue: "#0044CC",
+    woodSmoke: "#18181B",
+    grey: "#E4E6F0",
+    waterloo: "#7E839A",
+    black: "#000000",
+  },
+  breakpoint: {
+    mobileMax: 767,
+    tabletMax: 1080,
+  },
+};
